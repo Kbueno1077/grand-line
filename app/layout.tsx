14a9b08f04b219199a3291e45d6d8a33ada6b668
Kbueno1077 <kbueno1077@gmail.com>
2024-09-07 22:14:30 -0400

    viewport not zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from "geist/font/sans";
 import "./globals.css";
 import { Providers } from "./providers";
 import Footer from "@/components/Footer/Footer";
+import { Viewport } from "next";
 
 const defaultUrl = process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
@@ -12,6 +13,12 @@ export const metadata = {
     metadataBase: new URL(defaultUrl),
     title: "Next.js and Supabase Starter Kit",
     description: "The fastest way to build apps with Next.js and Supabase",
+};
+
+export const viewport: Viewport = {
+    initialScale: 1,
+    maximumScale: 1,
+    width: "device-width",
 };
 
 export default function RootLayout({
